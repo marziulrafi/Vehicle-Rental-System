0090568fd1b35875afb0e83eef5cd886f0926af2
K M Marziul Karim <marziulrafi@gmail.com>
2025-12-06 23:29:17 +0600

    Tables created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express, { Request, Response } from "express"
+import initDB from "./config/db";
+import config from "./config";
+
+
 const app = express()
-const port = 5000
+const port = config.port;
+
+
+app.use(express.json());
+
+initDB()
 
 app.get('/', (req: Request, res: Response) => {
     res.send('App running!')
